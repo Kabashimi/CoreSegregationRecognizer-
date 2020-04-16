26f1eef41fc92015e473b8954a3c237d52a68a5c
Johny_dudek <johny.dudek@gmail.com>
2020-04-16 14:17:14 +0200

    Set file write mode to append

--- Core Segregation Detector/FileUtility.cpp
+++ Core Segregation Detector/FileUtility.cpp
@@ -9,7 +9,7 @@ FileUtility::FileUtility()
 void FileUtility::WriteResultToFile(double size, double intensity, double segregation)
 {
 	std::ofstream myfile;
-	myfile.open("example.csv");
+	myfile.open("example.csv", std::ios::app);
 	myfile << size << ';' << intensity << ';' << segregation << std::endl;
 	myfile.close();
 
